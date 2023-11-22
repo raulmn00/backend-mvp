641b09da32958950bf4316a9fe9f89513805d617
Raul Mariaci Neto <raul.mariaci@italents.com.br>
2023-11-22 09:48:52 -0300

    feat: add fields for dtos

--- src/common/admin/dto/create-admin.dto.ts
+++ src/common/admin/dto/create-admin.dto.ts
@@ -1,1 +1,10 @@
-export class CreateAdminDto {}
+import { Prisma } from '@prisma/client';
+export class CreateAdminDto implements Prisma.AdminUncheckedCreateInput {
+  createdAt: Date | string;
+  credential: Prisma.CredentialUncheckedCreateNestedOneWithoutAdminInput;
+  email: string;
+  id: string;
+  messages: Prisma.MessageUncheckedCreateNestedManyWithoutAdminInput;
+  name: string;
+  phone: string;
+}
--- src/common/student/dto/create-student.dto.ts
+++ src/common/student/dto/create-student.dto.ts
@@ -1,1 +1,11 @@
-export class CreateStudentDto {}
+import { Prisma } from '@prisma/client';
+export class CreateStudentDto implements Prisma.StudentUncheckedCreateInput {
+  createdAt: Date | string;
+  credential: Prisma.CredentialUncheckedCreateNestedOneWithoutStudentInput;
+  email: string;
+  id: string;
+  messages: Prisma.MessageUncheckedCreateNestedManyWithoutStudentInput;
+  name: string;
+  phone: string;
+  tickets: Prisma.TicketUncheckedCreateNestedManyWithoutStudentInput;
+}
